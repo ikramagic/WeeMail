28a5b780827334d7d0e5e23ef7943cd482f85d2a
ikramiste <ikrame.saadi@gmail.com>
2023-11-22 16:19:24 +0100

    homepage - create email - render functionnal

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,10 +1,24 @@
+require 'faker'
+
 class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
 
+  def new
+    @email = Email.new
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
+
   def create
-    Email.create(object: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
-    redirect_to emails_path, notice: "Nouvel email !"
+    @email = Email.create(object: Faker::Quote.famous_last_words, body: Faker::Quote.famous_last_words)
+    render turbo_stream: turbo_stream.append('email-list', partial: 'emails/email', locals: { email: @email })
   end
 end
